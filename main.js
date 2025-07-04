c03642434151e90bfdf858bf555ebec855ea304d
PRINCE TECHNOLOGY <princerwanda2003@gmail.com>
2025-07-04 16:58:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -100,7 +100,7 @@ const videoCommand = require('./commands/video');
 global.packname = settings.packname;
 global.author = settings.author;
 global.channelLink = "https://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp3A";
-global.ytch = "Mr Unique Hacker";
+global.ytch = "princetech";
 
 // Add this near the top of main.js with other global configurations
 const channelInfo = {
@@ -109,7 +109,7 @@ const channelInfo = {
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
             newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterName: 'BWB XMD',
             serverMessageId: -1
         }
     }
